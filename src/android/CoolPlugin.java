893e5d92fcc4629ed589b5ef99fc8f56b287579b
PandaFoxx <kyleischampion@gmail.com>
2016-05-18 11:38:41 +0200

    Merge code from SDK with BarcodeScanner

--- src/android/CoolPlugin.java
+++ src/android/CoolPlugin.java
@@ -1,50 +1,105 @@
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaInterface;
-import android.util.Log;
-import android.provider.Settings;
-import android.widget.Toast;
+//package src.android;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
- 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+
+import com.honeywell.scanintent.ScanIntent;
+
+
 public class CoolPlugin extends CordovaPlugin {
- 
-	public static final String TAG = "Cool Plugin";
-	 
-	/**
-	* Constructor.
-	*/
-	public CoolPlugin() {}
-	 
-	/**
-	* Sets the context of the Command. This can then be used to do things like
-	* get file paths associated with the Activity.
-	*
-	* @param cordova The context of the main Activity.
-	* @param webView The CordovaWebView Cordova is running in.
-	*/
-	 
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		super.initialize(cordova, webView);
-		Log.v(TAG,"Init CoolPlugin");
+
+    /**
+     * Constructor.
+     */
+    public CoolPlugin() {
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+
+        if (action.equals(ENCODE)) {
+            JSONObject obj = args.optJSONObject(0);
+            if (obj != null) {
+                String type = obj.optString(TYPE);
+                String data = obj.optString(DATA);
+
+                // If the type is null then force the type to text
+                if (type == null) {
+                    type = TEXT_TYPE;
+                }
+
+                if (data == null) {
+                    callbackContext.error("User did not specify data to encode");
+                    return true;
+                }
+
+                encode(type, data);
+            } else {
+                callbackContext.error("User did not specify data to encode");
+                return true;
+            }
+        } else if (action.equals(SCAN)) {
+            scan(args);
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public void scan(JSONArray args) {
+    	Intent intentScan = new Intent(ScanIntent.SCAN_ACTION);
+		intentScan.addCategory(Intent.CATEGORY_DEFAULT);
+
+		intentScan.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intentScan.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+
+		int loadmode = 0;
+
+		intentScan.putExtra("scan_mode", ScanIntent.SCAN_MODE_RESULT_AS_URI);
+
+/*		SCAN_MODE_SHOW_NO_RESULT = 0;
+		SCAN_MODE_SHOW_RESULT_UI = 1;	
+		SCAN_MODE_SHARE_BY_SMS = 2;		
+		SCAN_MODE_SHARE_BY_MMS = 3;		
+		SCAN_MODE_SHARE_BY_EMAIL = 4;
+		SCAN_MODE_RESULT_AS_URI = 5;*/
+		
+		this.startActivityForResult(intentScan, 5);
+    }
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (resultCode == ScanIntent.SCAN_RESULT_SUCCESSED) {
+			String data = intent.getStringExtra(ScanIntent.EXTRA_RESULT_BARCODE_DATA);
+			int format = intent.getIntExtra(
+					ScanIntent.EXTRA_RESULT_BARCODE_FORMAT, 0);
+		
+			barcodeData.setText(ScanIntent.EXTRA_RESULT_BARCODE_DATA+ ": " + data + "\r\n" + ScanIntent.EXTRA_RESULT_BARCODE_FORMAT + ": " + format);
+		}
+		else
+			barcodeData.setText(R.string.scan_failed);
 	}
-	 
-	public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	         
-		final int duration = Toast.LENGTH_SHORT;
-		// Shows a toast
-		Log.v(TAG,"CoolPlugin received:"+ action);
-		 
-		         
-		cordova.getActivity().runOnUiThread(new Runnable() {
-			public void run() {
-				Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), action, duration);
-				toast.show();
-			}
-		});
-		 
-		return true;
-	}
+
+    public void encode(String type, String data) {
+        Intent intentEncode = new Intent(ENCODE_INTENT);
+        intentEncode.putExtra(ENCODE_TYPE, type);
+        intentEncode.putExtra(ENCODE_DATA, data);
+        // avoid calling other phonegap apps
+        intentEncode.setPackage(this.cordova.getActivity().getApplicationContext().getPackageName());
+
+        this.cordova.getActivity().startActivity(intentEncode);
+    }
 }
--- www/CoolPlugin.js
+++ www/CoolPlugin.js
@@ -4,16 +4,35 @@ function CoolPlugin() {
  console.log("CoolPlugin.js: is created");
 }
 
-CoolPlugin.prototype.showToast = function(aString){
- console.log("CoolPlugin.js: showToast");
+BarcodeScanner.prototype.scan = function (successCallback, errorCallback, config) {
 
- exec(function(result){
-     /*alert("OK" + reply);*/
-   },
-  function(result){
-    /*alert("Error" + reply);*/
-   },"CoolPlugin",aString,[]);
-}
+    if(config instanceof Array) {
+        // do nothing
+    } else {
+        if(typeof(config) === 'object') {
+            config = [ config ];
+        } else {
+            config = [];
+        }
+    }
+
+    if (errorCallback == null) {
+        errorCallback = function () {
+        };
+    }
+
+    if (typeof errorCallback != "function") {
+        console.log("CoolPlugin.scan failure: failure parameter not a function");
+        return;
+    }
+
+    if (typeof successCallback != "function") {
+        console.log("CoolPlugin.scan failure: success callback parameter must be a function");
+        return;
+    }
+
+    exec(successCallback, errorCallback, 'CoolPlugin', 'scan', config);
+};
 
  var coolPlugin = new CoolPlugin();
  module.exports = coolPlugin;
